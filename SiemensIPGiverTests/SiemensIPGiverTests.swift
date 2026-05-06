2fb0907019dbb034b08c9e5fbed06c9c33555e55
gmaendel <gmaendel@gmail.com>
2026-05-06 09:47:48 -0500

    Initial Commit

---
+++ SiemensIPGiverTests/SiemensIPGiverTests.swift
@@ -0,0 +1,17 @@
+//
+//  SiemensIPGiverTests.swift
+//  SiemensIPGiverTests
+//
+//  Created by Gregory Maendel on 5/6/26.
+//
+
+import Testing
+@testable import SiemensIPGiver
+
+struct SiemensIPGiverTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
